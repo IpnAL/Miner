b69f1a1ba24a2bb8080632b805f4b4cc5cc4d2b8
IpnAL <74337081+IpnAL@users.noreply.github.com>
2020-11-23 02:09:32 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
+git clone https://github.com/IpnAL/Miner.git
+cd Miner
+
+chmod +777 build.sh
+./build.sh
+clear
+
